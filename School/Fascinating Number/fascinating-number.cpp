1c56e0e92afcafcd209d5fe5137d05538e153c49
Neeraj Kumar <98253646+neerajcodes888@users.noreply.github.com>
2023-07-05 14:17:27 +0530

    Added Solution - GfG to GitHub

---
+++ School/Fascinating Number/fascinating-number.cpp
@@ -0,0 +1,43 @@
+//{ Driver Code Starts
+// Initial Template for C++
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+
+// } Driver Code Ends
+//User function template for C++
+class Solution{
+public:
+	
+	bool fascinating(int n) {
+	   int x=n*2;
+	   int y=n*3;
+	   string ans = to_string(n)+to_string(x)+to_string(y);
+	   sort(ans.begin(),ans.end());
+	   if(ans=="123456789")
+	   return true;
+	   return false;
+	}
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n;
+        cin >> n;
+        Solution ob;
+        auto ans = ob.fascinating(n);
+        if (ans) {
+            cout << "Fascinating\n";
+        } else {
+            cout << "Not Fascinating\n";
+        }
+    }
+    return 0;
+}
+// } Driver Code Ends
