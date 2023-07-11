f2c6f476bea8a55576c9323e981c4d0b641967f2
Neeraj Kumar <98253646+neerajcodes888@users.noreply.github.com>
2023-07-11 15:35:07 +0530

    Added Solution - GfG to GitHub

---
+++ Medium/Find kth element of spiral matrix/find-kth-element-of-spiral-matrix.cpp
@@ -0,0 +1,90 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+#define MAX 1000
+
+// } Driver Code Ends
+class Solution
+{
+    public:
+    /*You are required to complete this method*/
+    int findK(int matrix[MAX][MAX],int r,int c,int k)
+    {
+ 	
+   vector<int>ans;
+   
+        int left=0;
+        int right=c-1;
+        int top=0;
+        int bottom=r-1;
+        while(top<=bottom&&left<=right)
+        {
+            for(int i=left;i<=right;i++)
+            {
+                ans.push_back(matrix[top][i]);
+            }
+            top++;
+            
+            
+            for(int i=top;i<=bottom;i++)
+            {
+                ans.push_back(matrix[i][right]);
+            }
+            right--;
+            
+            
+            if(top<=bottom)
+            {
+                    for(int i=right;i>=left;i--)
+                    {
+                        ans.push_back(matrix[bottom][i]);
+                    }
+                    bottom--;
+            }
+            
+            
+            if(left<=right)
+            {
+                for(int i=bottom;i>=top;i--)
+                {
+                    ans.push_back(matrix[i][left]);
+                }
+                left++;
+            }
+        }
+        return ans[k-1];
+    }
+};
+
+
+
+
+
+//{ Driver Code Starts.
+int main()
+{
+    int T;
+    cin>>T;
+  
+    while(T--)
+    {
+        int n,m;
+        int k=0;
+        //cin>>k;
+        cin>>n>>m>>k;
+        int a[MAX][MAX];
+        
+        for(int i=0;i<n;i++)
+        {
+            for(int j=0;j<m;j++)
+            {
+                cin>>a[i][j];
+            }
+        }
+        Solution ob;
+        cout<<ob.findK(a,n,m,k)<<endl;
+        
+       
+    }
+}
+// } Driver Code Ends
