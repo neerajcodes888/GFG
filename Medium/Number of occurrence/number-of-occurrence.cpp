3263ce40bdbeb35ed8cc89676fb0dad2ec151bf8
Neeraj Kumar <98253646+neerajcodes888@users.noreply.github.com>
2023-08-20 21:32:44 +0530

    Updated Solution - GfG to GitHub

--- Medium/Number of occurrence/number-of-occurrence.cpp
+++ Medium/Number of occurrence/number-of-occurrence.cpp
@@ -11,10 +11,13 @@ public:
 
 	int count(int arr[], int n, int x) {
 	    int c=0;
-for(int i=0;i<n;i++)
+	    map<int,int>m;
+	    for(int i=0;i<n;i++)
+	    ++m[arr[i]];
+for(auto it:m)
 {
-    if(arr[i]==x)
-    c++;
+    if(it.first==x)
+    c=it.second;
 }
 return c;
 	}
