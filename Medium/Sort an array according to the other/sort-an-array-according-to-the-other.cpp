32fa0d3dddc52eff90bf811a298afd1f4991718e
Neeraj Kumar <98253646+neerajcodes888@users.noreply.github.com>
2023-08-27 11:52:20 +0530

    Added Solution - GfG to GitHub

---
+++ Medium/Sort an array according to the other/sort-an-array-according-to-the-other.cpp
@@ -0,0 +1,86 @@
+//{ Driver Code Starts
+//Initial template for C++
+
+#include <bits/stdc++.h> 
+using namespace std;
+
+
+// } Driver Code Ends
+//User function template in C++
+
+
+class Solution{
+    public:
+    // A1[] : the input array-1
+    // N : size of the array A1[]
+    // A2[] : the input array-2
+    // M : size of the array A2[]
+    
+    //Function to sort an array according to the other array.
+    vector<int> sortA1ByA2(vector<int> A1, int N, vector<int> A2, int M) 
+    {
+        map<int,int>mp;
+        vector<int>ans;
+        for(auto it:A1)
+        ++mp[it];
+        for(auto it:A2)
+        {
+            if(mp.find(it)!=mp.end())
+            {
+            auto a = mp.find(it)->second;
+                vector<int>v(a,it);
+                ans.insert(ans.end(),v.begin(),v.end());
+                mp.erase(it);
+            }
+        }
+        for(auto i:mp)
+        {
+            auto it = i.first;
+            auto a = i.second;
+             vector<int>v(a,it);
+                ans.insert(ans.end(),v.begin(),v.end());
+        }
+        return ans;
+    } 
+};
+
+//{ Driver Code Starts.
+
+int main(int argc, char *argv[]) 
+{ 
+	
+	int t;
+	
+	cin >> t;
+	
+	while(t--){
+	    
+	    int n, m;
+	    cin >> n >> m;
+	    
+	    vector<int> a1(n);
+	    vector<int> a2(m);
+	    
+	    for(int i = 0;i<n;i++){
+	        cin >> a1[i];
+	    }
+	    
+	    for(int i = 0;i<m;i++){
+	        cin >> a2[i];
+	    }
+	    
+	    Solution ob;
+	    a1 = ob.sortA1ByA2(a1, n, a2, m); 
+	
+	   
+	    for (int i = 0; i < n; i++) 
+		    cout<<a1[i]<<" ";
+		
+	    cout << endl;
+	    
+	    
+	}
+	return 0; 
+} 
+
+// } Driver Code Ends
