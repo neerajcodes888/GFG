2737595b0f5c57aea899739842a5f6dc8f07f46c
Neeraj Kumar <98253646+neerajcodes888@users.noreply.github.com>
2024-01-09 00:29:13 +0530

    Added Solution - GfG to GitHub

---
+++ Medium/Subarray with 0 sum/subarray-with-0-sum.cpp
@@ -0,0 +1,52 @@
+//{ Driver Code Starts
+// A C++ program to find if there is a zero sum
+// subarray
+#include <bits/stdc++.h>
+using namespace std;
+
+
+
+// } Driver Code Ends
+class Solution{
+    public:
+    //Complete this function
+    //Function to check whether there is a subarray present with 0-sum or not.
+    bool subArrayExists(int arr[], int n)
+    {
+      set<int>st;
+      int sum=0;
+      for(int i=0;i<n;i++)
+      {
+          sum+=arr[i];
+          if(sum==0)
+          return 1;
+          if(st.find(sum)!=st.end())
+          return 1;
+          st.insert(sum);
+      }
+      return 0;
+    }
+};
+
+//{ Driver Code Starts.
+// Driver code
+int main()
+{
+	int t;
+	cin>>t;
+	while(t--)
+	{
+	    int n;
+	    cin>>n;
+	    int arr[n];
+	    for(int i=0;i<n;i++)
+	    cin>>arr[i];
+	    Solution obj;
+	    	if (obj.subArrayExists(arr, n))
+		cout << "Yes\n";
+	else
+		cout << "No\n";
+	}
+	return 0;
+}
+// } Driver Code Ends
