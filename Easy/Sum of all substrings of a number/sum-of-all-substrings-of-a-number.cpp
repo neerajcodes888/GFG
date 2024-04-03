c77e451522d344b63eb0bb1a141bfb97912109af
Neeraj Kumar <neerajmail888@gmail.com>
2024-04-04 00:00:49 +0530

    Added Solution - GfG to GitHub

---
+++ Easy/Sum of all substrings of a number/sum-of-all-substrings-of-a-number.cpp
@@ -0,0 +1,46 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+
+class Solution
+{
+    public:
+    //Function to find sum of all possible substrings of the given string.
+    long long sumSubstrings(string s){
+        
+       long long int mod=1e9+7;
+        long long int r=1,res=0;
+            for(int i=s.size()-1;i>=0;i--){
+            // long long int  
+            res=(res+((s[i]-'0')*(i+1)*r)%mod)%mod;
+            res%=mod;
+            r=(r*10+1)%mod;
+            r%=mod;
+            
+        }
+        return(res);
+    }
+};
+
+//{ Driver Code Starts.
+int main()
+{
+    //taking total testcases
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        //taking string
+        string s;
+        cin>>s;
+        Solution ob;
+        //calling sumSubstrings() function
+        cout << ob.sumSubstrings(s) << endl;
+        
+    }
+    return 0;
+}
+
+// } Driver Code Ends
