ac429fe2c9f913c2d83d81528c1d817a66749c0f
Neeraj Kumar <neerajmail888@gmail.com>
2024-04-28 00:01:26 +0530

    Added Solution - GfG to GitHub

---
+++ Easy/Delete Middle of Linked List/delete-middle-of-linked-list.cpp
@@ -0,0 +1,102 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h> 
+using namespace std; 
+struct Node
+{
+    int data;
+    struct Node* next;
+    
+    Node(int x){
+        data = x;
+        next = NULL;
+    }
+};
+
+
+
+// } Driver Code Ends
+/* Link list Node:
+
+struct Node
+{
+    int data;
+    struct Node* next;
+
+    Node(int x){
+        data = x;
+        next = NULL;
+    }
+};
+
+*/
+
+// Deletes middle of linked list and returns head of the modified list
+class Solution{
+    public:
+    Node* deleteMid(Node* head)
+    {
+          if(head->next==NULL){
+             delete head;
+            return head;
+        }
+        struct Node* temp=head;
+        int count=1;
+        while(temp->next!=NULL){
+            temp=temp->next;
+            count++;
+        }
+        temp=NULL;
+        int median=(count/2)+1;
+        struct Node* del=head;
+        for(int i=1;i<median;i++){
+            temp=del;
+            del=del->next;
+        }
+        temp->next=del->next;
+        del->next=NULL;
+        delete del;
+        return head;
+    }
+};
+
+//{ Driver Code Starts.
+
+
+
+void printList(Node* node) 
+{ 
+	while (node != NULL) { 
+		cout << node->data <<" "; 
+		node = node->next; 
+	}  
+	cout<<"\n";
+} 
+int main() 
+{ 
+	int t;
+	cin>>t;
+	while(t--)
+	{
+		int n;
+		cin>>n;
+
+		int data;
+		cin>>data;
+		struct Node *head = new Node(data);
+		struct Node *tail = head;
+		for (int i = 0; i < n-1; ++i)
+		{
+			cin>>data;
+			tail->next = new Node(data);
+			tail = tail->next;
+		}
+		Solution obj;
+		head = obj.deleteMid(head);
+		printList(head); 
+	}
+	return 0; 
+} 
+
+
+
+// } Driver Code Ends
