01011072648739f3cd423ce770bb450919064290
Neeraj Kumar <neerajmail888@gmail.com>
2024-05-12 10:42:16 +0530

    Added Solution - GfG to GitHub

---
+++ Medium/Minimum steps to destination/minimum-steps-to-destination.cpp
@@ -0,0 +1,35 @@
+//{ Driver Code Starts
+// Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+// User function Template for C++
+
+class Solution {
+  public:
+    int minSteps(int d) {
+         int x  = ceil((-1+sqrt(1+8*d))/2);
+        if((x*(x+1)/2-d)%2==0) return x;
+        if(x%2==0) return x+1;
+        return x+2;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int d;
+        cin >> d;
+
+        Solution ob;
+        cout << ob.minSteps(d) << "\n";
+    }
+    return 0;
+}
+// } Driver Code Ends
