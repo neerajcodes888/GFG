8db825831f84d2df57fee527e2b049fda9ac2810
Neeraj Kumar <neerajmail888@gmail.com>
2024-05-22 00:30:29 +0530

    Update minimize-max-distance-to-gas-station.cpp

--- Hard/Minimize Max Distance to Gas Station/minimize-max-distance-to-gas-station.cpp
+++ Hard/Minimize Max Distance to Gas Station/minimize-max-distance-to-gas-station.cpp
@@ -6,8 +6,33 @@ using namespace std;
 // } Driver Code Ends
 class Solution {
   public:
-    double findSmallestMaxDist(vector<int> &stations, int k) {
-        // Code here
+     bool isPossible(vector<int>& s, double mid, int k) {
+        int cnt = 0;
+        int n = s.size();
+        for (int i = 1; i < n; ++i) {
+            cnt += ceil((s[i] - s[i - 1]) / mid) - 1;
+        }
+        return cnt <= k;
+    }
+
+    double findSmallestMaxDist(vector<int>& s, int k) {
+        sort(s.begin(), s.end());
+        
+        double st = 0.0;
+        double en = s.back() - s.front(); 
+        
+        double ans = en;
+        
+        while (en - st > 1e-6) {
+            double mid = st + (en - st) / 2.0;
+            if (isPossible(s, mid, k)) {
+                ans = mid;
+                en = mid;
+            } else {
+                st = mid;
+            }
+        }
+        return ans;
     }
 };
 
