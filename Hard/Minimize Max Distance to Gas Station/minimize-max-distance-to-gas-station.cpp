2ecb1a459847570ac005b3b119eb7ad1d6ce8b11
Neeraj Kumar <neerajmail888@gmail.com>
2024-05-22 00:29:05 +0530

    Added Solution - GfG to GitHub

---
+++ Hard/Minimize Max Distance to Gas Station/minimize-max-distance-to-gas-station.cpp
@@ -0,0 +1,33 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+class Solution {
+  public:
+    double findSmallestMaxDist(vector<int> &stations, int k) {
+        // Code here
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n;
+        cin >> n;
+        vector<int> stations(n);
+        for (int i = 0; i < n; i++) {
+            cin >> stations[i];
+        }
+        int k;
+        cin >> k;
+        Solution obj;
+        cout << fixed << setprecision(2) << obj.findSmallestMaxDist(stations, k)
+             << endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
